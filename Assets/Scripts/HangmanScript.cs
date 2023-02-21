cd48c8fa6d8ba1efd7d067c91100c35f89c3b4c1
VadimGera <vadimnvidia@gmail.com>
2023-02-21 16:18:43 +0300

    Refactor.

--- Assets/Scripts/HangmanScript.cs
+++ Assets/Scripts/HangmanScript.cs
@@ -1,127 +1,117 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class HangmanScript : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _textField;
-    [SerializeField] private TextMeshProUGUI _hintsTextField;
+    [SerializeField] private TextMeshProUGUI textField;
+    [SerializeField] private TextMeshProUGUI hintsTextField;
     [SerializeField] private int hp = 6;
-    [SerializeField] private TextMeshProUGUI _hpCurrent;
+    [SerializeField] private TextMeshProUGUI hpCurrent;
     [SerializeField] private GameObject restartPanelLose;
     [SerializeField] private GameObject restartPanelWin;
+
+    private readonly List<char> _guessedLetters = new();
+    private readonly List<char> _wrongTriedLetter = new();
+
+    private readonly string[] _words =
+    {
+        "Tree",
+        "Dog",
+        "Rain",
+        "Program",
+        "PC",
+        "Snow",
+        "Phone"
+    };
+
+    private readonly string[] _hints =
+    {
+        "green leaves in summer",
+        "walking on the street",
+        "in bad autumn weather",
+        "need to make it work",
+        "we are working on it",
+        "outdoors in winter",
+        "you can call it"
+    };
+
+    private string _wordToGuess = "";
+    private string _hint = "";
+
+    private KeyCode _lastKeyPressed;
+
+    private void Start()
+    {
+        var randomIndex = Random.Range(0, _words.Length);
+
+        _wordToGuess = _words[randomIndex];
+        _hint = _hints[randomIndex];
+        hintsTextField.text = _hint;
+    }
+
+
+    private void OnGUI()
+    {
+        var e = Event.current;
+        if (!e.isKey) return;
+        if (e.keyCode == KeyCode.None || _lastKeyPressed == e.keyCode) return;
+        ProcessKey(e.keyCode);
+
+        _lastKeyPressed = e.keyCode;
+    }
+
+    private void ProcessKey(KeyCode key)
+    {
+        print("Key Pressed: " + key);
+
+        var pressedKeyString = key.ToString()[0];
+
+        var wordUppercase = _wordToGuess.ToUpper();
+
+        var wordContainsPressedKey = wordUppercase.Contains(pressedKeyString);
+        var letterWasGuessed = _guessedLetters.Contains(pressedKeyString);
+
+        if (!wordContainsPressedKey && !_wrongTriedLetter.Contains(pressedKeyString))
+        {
+            _wrongTriedLetter.Add(pressedKeyString);
+            hp -= 1;
+
+
+            if (hp <= 0)
+            {
+                restartPanelLose.SetActive(true);
+            }
+            else
+            {
+                hpCurrent.text = hp.ToString();
+            }
+        }
+
+        if (wordContainsPressedKey && !letterWasGuessed)
+        {
+            _guessedLetters.Add(pressedKeyString);
+        }
+
+        var stringToPrint = "";
+        foreach (var letterInWord in wordUppercase)
+        {
+            if (_guessedLetters.Contains(letterInWord))
+            {
+                stringToPrint += letterInWord;
+            }
+            else
+            {
+                stringToPrint += "_";
+            }
+        }
+
+        if (wordUppercase == stringToPrint)
+        {
+            restartPanelWin.SetActive(true);
+        }
         
-        private List<char> guessedLetters = new List<char>();
-        private List<char> wrongTriedLetter = new List<char>();
-
-        private string[] words =
-        {
-            "Tree",
-            "Dog",
-            "Rain",
-            "Program",
-            "PC",
-            "Snow",
-            "Phone"
-        };
-
-        private string[] hints =
-        {
-            "green leaves in summer",
-            "walking on the street",
-            "in bad autumn weather",
-            "need to make it work",
-            "we are working on it",
-            "outdoors in winter",
-            "you can call it"
-        };
-
-        private string wordToGuess = "";
-        private string hint = "";
-        
-        private KeyCode lastKeyPressed;
-
-        private void Start()
-        {
-            var randomIndex = Random.Range(0, words.Length);
-
-            wordToGuess = words[randomIndex];
-            hint = hints[randomIndex];
-            _hintsTextField.text = hint;
-        }
-
-
-        void OnGUI()
-        {
-            Event e = Event.current;
-            if (e.isKey)
-            {
-                // Debug.Log("Detected key code: " + e.keyCode);
-
-                if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode)
-                {
-                    ProcessKey(e.keyCode);
-                    
-                    lastKeyPressed = e.keyCode;
-                }
-            }
-        }
-
-        private void ProcessKey(KeyCode key)
-        {
-            print("Key Pressed: " + key);
-
-            var pressedKeyString = key.ToString()[0];
-
-            var wordUppercase = wordToGuess.ToUpper();
-            
-            var wordContainsPressedKey = wordUppercase.Contains(pressedKeyString);
-            bool letterWasGuessed = guessedLetters.Contains(pressedKeyString);
-
-            if (!wordContainsPressedKey && !wrongTriedLetter.Contains(pressedKeyString))
-            {
-                wrongTriedLetter.Add(pressedKeyString);
-                hp -= 1;
-                
-
-                if (hp <= 0)
-                {
-                    restartPanelLose.SetActive(true);
-                }
-                else
-                {
-                    _hpCurrent.text = hp.ToString();
-                }
-            }
-            
-            if (wordContainsPressedKey && !letterWasGuessed)
-            {
-                guessedLetters.Add(pressedKeyString);
-            }
-
-            string stringToPrint = "";
-            for (int i = 0; i < wordUppercase.Length; i++)
-            {
-                char letterInWord = wordUppercase[i];
-
-                if (guessedLetters.Contains(letterInWord))
-                {
-                    stringToPrint += letterInWord;
-                }
-                else
-                {
-                    stringToPrint += "_";
-                }
-            }
-
-            if (wordUppercase == stringToPrint)
-            {
-                restartPanelWin.SetActive(true);
-            }
-            
-            // print(string.Join(", ", guessedLetters));
-            print(stringToPrint);
-            _textField.text = stringToPrint;
-        }
+        print(stringToPrint);
+        textField.text = stringToPrint;
+    }
 }
